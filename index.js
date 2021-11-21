4c1269ee6112a3ae9ce49953d6f78b05d6d4011b
Theophile Kango <fadhili.kango@gmail.com>
2021-11-21 15:22:59 +0200

    Add nodemon and read files from the server

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const http = require('http');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req,res) => {
+    const readFile = path => {
+        try{
+            return fs.readFileSync(path, 'utf-8')
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    switch(req.url){
+        case "/" : 
+            res.end(readFile('index.html'));
+            break;
+        case "/about":
+            res.end(readFile('about.html'));
+            break;
+        case "/contact":
+            res.end(readFile('contact-me.html'));
+            break;
+        default:
+            res.end(readFile('404.html'));
+    }
+});
+//const server = http.createServer((req,res) => res.send("Okay"))
+server.listen(port, () => console.log(`The server is runing on port ${port}`))
